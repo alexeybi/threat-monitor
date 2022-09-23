5a16a808103957ffd8b56a352bdba88117e0f04c
alexeybi <alexey.bityukov@code.berlin>
2022-09-23 11:09:39 +0200

    Fix tshark command

--- server/src/main/scala/server/data/HttpTlsData.scala
+++ server/src/main/scala/server/data/HttpTlsData.scala
@@ -6,8 +6,8 @@ import server.data.Data. delayedCmd
 object HttpTlsData:
   def rawData[F[_]: Sync](interface: String): F[String] =
     delayedCmd(
-      "tshark"                                                                               +
-        s" -l -i $interface"                                                                               +
+      "sudo tshark"                                                                          +
+        s" -l -i $interface"                                                                       +
         " -t a"                                                                                    +
         " -T fields -e tls.handshake.extensions_server_name -e ip.dst -e ipv6.dst -e _ws.col.Time" +
         " -Y 'tls.handshake.extension.type == 0'"                                                  +
